f21e0e5476db89b7ad274b018b518423cda7b1c9
pranta <you@example.com>
2024-10-24 16:58:56 +0600

    update route in clear

--- src/scrapers/index.js
+++ src/scrapers/index.js
@@ -1,3 +1,25 @@
-// require("./prothomAlo.com/latestNewsPage")
+const scrapeProthomAlo = require("./prothomAlo.com/latestNewsPage");
 
+const waitHere = () => {
+  let intervalTime = 10 * 60 * 1000; // 5 minutes
 
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("done");
+    }, intervalTime);
+  });
+};
+
+const startScrape = async () => {
+  try {
+    await scrapeProthomAlo();
+
+    console.log("hello after scrap");
+    await waitHere();
+    await startScrape();
+  } catch (error) {
+    console.log("Error form startScrape :-", error);
+  }
+};
+
+startScrape();
--- src/scrapers/prothomAlo.com/latestNewsPage.js
+++ src/scrapers/prothomAlo.com/latestNewsPage.js
@@ -1,15 +1,20 @@
 const gotoPage = require("../shared/utilities/gotoPage");
-const saveToCollectedNews = require("../../shared/utilize/saveToCollectedNews")
+const saveToCollectedNews = require("../../shared/utilize/saveToCollectedNews");
+const CollectedNewsCollection = require("../../DB/Modals/collectedNews");
+const NewsCollection = require("../../DB/Modals/news");
 const remoteContentText = ["আরও পড়ুন", "বিজ্ঞাপন"];
 
 const scrapeProthomAlo = async () => {
-  const { page, browser } = await gotoPage('https://www.prothomalo.com/collection/latest');
-  const pageEvaluate = await page.evaluate(() => {
-    let articles = [{ pageUrl: "https://www.prothomalo.com/entertainment/tollywood/hscg3knw9a" }];
-    const contentContainer = document.querySelectorAll('.xkXol');
+  console.log("Start Scrape on Phothomalo");
+  const { page, browser } = await gotoPage(
+    "https://www.prothomalo.com/collection/latest"
+  );
+  let pageEvaluate = await page.evaluate(() => {
+    let articles = [ ];
+    const contentContainer = document.querySelectorAll(".xkXol");
 
-    contentContainer.forEach(article => {
-      const pageUrl = article.querySelector('a')?.href;
+    contentContainer.forEach((article) => {
+      const pageUrl = article.querySelector("a")?.href;
       if (pageUrl) {
         articles.push({ pageUrl });
       }
@@ -18,97 +23,121 @@ const scrapeProthomAlo = async () => {
     return articles;
   });
 
+  pageEvaluate = await Promise.all(
+    pageEvaluate.filter(async (pageInfo) => {
+      if (!pageInfo.pageUrl) {
+        return false;
+      }
+      const existInCollectedNews = await CollectedNewsCollection.findOne({
+        sourceUrl: pageInfo.pageUrl,
+      }).select("_id");
+      if (existInCollectedNews) {
+        return false;
+      }
+      const existIndNews = await NewsCollection.findOne({
+        sourceUrl: pageInfo.pageUrl,
+      }).select("_id");
+      if (existIndNews) {
+        return false;
+      }
 
-  let contentList = await (await Promise.all(pageEvaluate.map(async (pageInfo) => {
-    const { page, browser } = await gotoPage(pageInfo.pageUrl);
-    const newsDetailsPageEvaluate = await page.evaluate(async (pageUrl) => {
-      let articles = {};
+      return true;
+    })
+  );
 
-      const titleElements = await document.querySelector('.IiRps')
-      if (!titleElements) {
-        return
-      }
-      const title = await titleElements?.innerText
-      
-      const newsContainer = await document.querySelector('.story-content');
-      if (!newsContainer) {
-        return {};
-      }
-      const readMoreElements = newsContainer.querySelectorAll('.also-read');
-      const adsElements = newsContainer.querySelectorAll('.adsBox');
+  console.log("pageEvaluate ==>>", pageEvaluate)
 
-      if (readMoreElements && readMoreElements.length) {
-        readMoreElements.forEach(element => {
-          element.remove();
-        });
-      }
-      if (adsElements && adsElements.length) {
-        adsElements.forEach(element => {
-          element.remove();
-        });
-      }
-      const imageElements = await newsContainer.querySelectorAll('img')
-      if (!imageElements || !imageElements.length) {
-        return articles
-      }
-      const images = await Array.from(imageElements).map(img => {
-        return {
-          src: img?.src || "",
-          alt: img?.alt || ""
+  let contentList = await await Promise.all(
+    pageEvaluate.map(async (pageInfo) => {
+      const { page, browser } = await gotoPage(pageInfo.pageUrl);
+      const newsDetailsPageEvaluate = await page.evaluate(async (pageUrl) => {
+        let articles = {};
+
+        const titleElements = await document.querySelector(".IiRps");
+        if (!titleElements) {
+          return;
         }
-      });
+        const title = await titleElements?.innerText;
 
-      const contentBody = await newsContainer.querySelector('.VzzDZ');
-      if (!contentBody) {
-        return {};
-      }
-      const paragraphsElements = await contentBody.querySelectorAll("p");
-      if (!paragraphsElements || !paragraphsElements.length) {
-        return
-      }
+        const newsContainer = await document.querySelector(".story-content");
+        if (!newsContainer) {
+          return {};
+        }
+        const readMoreElements = newsContainer.querySelectorAll(".also-read");
+        const adsElements = newsContainer.querySelectorAll(".adsBox");
 
-      let htmlDescription = "";
-      await paragraphsElements.forEach(p => {
-        htmlDescription += p.outerHTML;
-      });
+        if (readMoreElements && readMoreElements.length) {
+          readMoreElements.forEach((element) => {
+            element.remove();
+          });
+        }
+        if (adsElements && adsElements.length) {
+          adsElements.forEach((element) => {
+            element.remove();
+          });
+        }
+        const imageElements = await newsContainer.querySelectorAll("img");
+        if (!imageElements || !imageElements.length) {
+          return articles;
+        }
+        const images = await Array.from(imageElements).map((img) => {
+          return {
+            src: img?.src || "",
+            alt: img?.alt || "",
+          };
+        });
 
-      let category = ""
-      let subCategory = ""
-      const domain = "https://www.prothomalo.com/"
-      let url = pageUrl
+        const contentBody = await newsContainer.querySelector(".VzzDZ");
+        if (!contentBody) {
+          return {};
+        }
+        const paragraphsElements = await contentBody.querySelectorAll("p");
+        if (!paragraphsElements || !paragraphsElements.length) {
+          return;
+        }
 
-      url = url.replace(domain, "")
-      const pathList = url.split("/")
-      category = pathList[0]
-      subCategory = pathList[1]
-      if (title && images.length && htmlDescription) {
-        articles = {
-          title,
-          htmlDescription,
-          images,
-          category,
-          subCategory,
-          sourceUrl: pageUrl,
-        };
-      }
-      return articles;
-    }, pageInfo.pageUrl);
+        let htmlDescription = "";
+        await paragraphsElements.forEach((p) => {
+          htmlDescription += p.outerHTML;
+        });
 
-    return newsDetailsPageEvaluate;
-  })))
+        let category = "";
+        let subCategory = "";
+        const domain = "https://www.prothomalo.com/";
+        let url = pageUrl;
 
-  contentList = await contentList.filter(((content, index) => {
+        url = url.replace(domain, "");
+        const pathList = url.split("/");
+        category = pathList[0];
+        subCategory = pathList[1];
+        if (title && images.length && htmlDescription) {
+          articles = {
+            title,
+            htmlDescription,
+            images,
+            category,
+            subCategory,
+            sourceUrl: pageUrl,
+          };
+        }
+        return articles;
+      }, pageInfo.pageUrl);
+
+      return newsDetailsPageEvaluate;
+    })
+  );
+
+  contentList = await contentList.filter((content, index) => {
     if (!content || !Object.keys(content || {}).length) {
-      return false
+      return false;
     }
-    return true
-  }))
+    return true;
+  });
 
   await contentList.map(async (content, index) => {
-    await saveToCollectedNews(content)
-  })
+    await saveToCollectedNews(content);
+  });
   await global.browser.close();
-
 };
 
-module.exports = scrapeProthomAlo();
+module.exports = scrapeProthomAlo;
