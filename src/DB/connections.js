40b56a7f6cf0b5af984df60b989d95fcb762b918
Symul Kabir <symulkabirpranta@gmail.com>
2024-12-21 22:09:09 +0600

    fix database url issue

--- src/DB/connections.js
+++ src/DB/connections.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const URI = `mongodb+srv://saimonpranta:${process.env.DB_PASSWORD}@somacharcluster.vn9e3.mongodb.net/somachar?retryWrites=true&w=majority`;
-const URI = "mongodb://localhost:27017/somachar"
+const URI = `mongodb+srv://saimonpranta:${process.env.DB_PASSWORD}@somacharcluster.vn9e3.mongodb.net/somachar?retryWrites=true&w=majority`;
+// const URI = "mongodb://localhost:27017/somachar"
 mongoose.set("strictQuery", false);
 mongoose.connect(URI).then((success) => {
   console.log("successfully connected with database.");
